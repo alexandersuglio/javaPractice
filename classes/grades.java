d663a1a8441d90131b8a7e3dda66804b869a9acc
Alex Suglio <alexandersuglio@gmail.com>
2018-05-08 22:22:45 -0400

    added practice with classes

---
+++ classes/grades.java
@@ -0,0 +1,19 @@
+class grades{
+  
+  private String courseName;
+  
+  public grades(String name) 
+  {
+    courseName = name;
+  }
+  
+  public void setName(String nameset){
+    courseName = nameset;
+  }
+  
+  public String getName()
+  {
+    return courseName;
+  }
+  
+}
