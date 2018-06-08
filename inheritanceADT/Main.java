e417e204847bc1e4ee7bf8b3447ffc264f2af300
alexandersuglio <alexandersuglio@gmail.com>
2018-06-08 06:33:48 -0400

    pushed latest before work

--- inheritanceADT/Main.java
+++ inheritanceADT/Main.java
@@ -1,21 +1,31 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-
+ 
 //can NOT call object of abstract class
 // animal test = new animal("Cat", 4);
 // System.out.println(test.getName());
 
-dog dogObj = new dog("Ruby", 4, false);
+dog dogObj = new dog("unnamed", 4, false);
 System.out.println(dogObj.tailOrNah());
 System.out.println(dogObj.getName());
+System.out.println(dogObj.greeting());
+
+pug pugObj = new pug("Ruby", 3, false, "Pug" );
+System.out.println(pugObj.greeting());
 
 
 bird birdObj = new bird("Tweety", 2, true);
 System.out.println(birdObj.getName());
 System.out.println(birdObj.beakOrNah());
+System.out.println(birdObj.greeting());
 
-  }
+
+snake snakeObj = new snake("Sebastian", 0, true);
+System.out.println(snakeObj.getName());
+System.out.println(snakeObj.forkedTongueOrNah());
+System.out.println(snakeObj.greeting());
+
+ }
 }
 
 abstract class animal
@@ -49,6 +59,11 @@ abstract class animal
     return name;
   }
 
+  public String greeting()
+  {
+    return "Hello! I am an animal named " + getName();
+  }
+
 }
 
 
@@ -64,7 +79,44 @@ class dog extends animal{
   {
     return true;
   }
+
+    @Override
+  public String greeting()
+  {
+    return "Hola, I am a dog named " + getName();
+  }
+
 }
+
+
+class pug extends dog{
+  private String breed;
+  private boolean snorts;
+
+public pug(String name, int legs, boolean snorts, String breed){
+  super( name,  legs,  snorts);  
+ // this.snorts = snorts;
+  this.breed = breed;
+}
+
+public boolean snortsOrNah(){
+  return true;
+}
+
+public String getBreed()
+{
+  return breed;
+}
+
+@Override
+public String greeting(){
+  return "Hey guys! My name is " + getName() + " and I am a " + getBreed(); 
+}
+
+
+}
+
+
 
 class bird extends animal{
   private boolean beak;
@@ -78,6 +130,30 @@ class bird extends animal{
   {
     return true;
   }
+
+    @Override
+  public String greeting()
+  {
+    return "Hola, I am a bird named " + getName();
+  }
+
+
+}
+
+
+class snake extends animal{
+  private boolean forkedTongue;
+  public snake(String name, int legs, boolean forkedTongue)
+  {
+    super(name, legs);
+    this.forkedTongue = forkedTongue;
+  }
+
+  public boolean forkedTongueOrNah()
+  {
+    return true;
+  }
+
 
 
 }
