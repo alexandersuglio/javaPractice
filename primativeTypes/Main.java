ca53d41f26b667ff47525a8bd2ca702aa76dee2e
alexandersuglio <alexandersuglio@gmail.com>
2018-07-23 23:46:10 -0400

    added primative type practice

---
+++ primativeTypes/Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+boolean dog = true;
+System.out.println(dog);
+
+Integer a = 13;
+
+
+System.out.println(a);
+
+int x = 1, y = 2, z =3;
+z = y = x;
+
+System.out.println(z);
+
+
+int rr = 6;
+
+System.out.println(++rr);
+  }
+}
