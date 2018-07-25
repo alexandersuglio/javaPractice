bc0b3c1bd5e9b19ac23e2d52c9efc8d4d522ab95
Alex Suglio <alexandersuglio@gmail.com>
2018-07-24 23:21:20 -0400

    added practice on wrapper classes

---
+++ wrapperClass/Main 3.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+class Main
+{
+    public static void main(String[] args)
+    {
+
+
+int baseDecimal = 267;
+int octVal = 0413;
+int hexVal = 0x10B;
+int binVal = 0b100001011;
+
+System.out.println(octVal + baseDecimal);
+
+System.out.println(Integer.toBinaryString(binVal));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // char ch = 'a';
+ 
+        // // Autoboxing- primitive to Character object conversion
+        // Character a = ch;
+ 
+        // ArrayList<Integer> arrayList = new ArrayList<Integer>();
+ 
+        // // Autoboxing because ArrayList stores only objects
+        // arrayList.add(25);
+ 
+        // // printing the values from object
+        // System.out.println(arrayList.get(0));
+
+String name = "Alex";
+
+// long f3 = 327__68;
+
+// System.out.println(f3);
+
+        char x = 'x';
+
+        Character charObj = new Character(x);
+    }
+}
